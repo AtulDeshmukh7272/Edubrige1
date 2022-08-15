389b3de0b1e9b3a23552072ef4a3da691f2a19ae
user <user@LAPTOP-Q2H26KJO>
2022-08-15 07:40:09 +0530

    commit spring

---
+++ rest-api-for-student/src/main/java/com/student/RestApiForStudentApplication.java
@@ -0,0 +1,13 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiForStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiForStudentApplication.class, args);
+	}
+
+}
